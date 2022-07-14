6f5d74ea1a72efe831cdb162b1881e289d5a94e4
DilanAtomica <dilan-shwane@hotmail.com>
2022-07-14 19:58:21 +0200

    Added the skills section with the different icons

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import "./HomePage.css";
 import HomePanel from "../../Components/HomePage/HomePanel/HomePanel";
+import AboutContainer from "../../Components/HomePage/AboutContainer/AboutContainer";
+import { AiFillHtml5 } from 'react-icons/ai';
+import { IoLogoCss3 } from 'react-icons/io';
+import { IoLogoJavascript } from 'react-icons/io5';
+import { IoLogoReact } from 'react-icons/io5';
+import { FaJava } from 'react-icons/fa';
+import { FaDatabase } from 'react-icons/fa';
+import { IoLogoFirebase } from 'react-icons/io5';
+import { FaGitAlt } from 'react-icons/fa';
 
 function HomePage(props) {
 
     return (
         <main className="homePage">
             <HomePanel />
+            <AboutContainer />
 
-            <div className="aboutContainer">
-                <h1>About</h1>
-                <p>My name is Dilan and I'm a 23 year old student living in Kristiansand, Norway. Im currently in my third year at University of Agder, studying IT and information-systems.
-                I have been learning frontend development next to school, every day since Januray of 2022. I love learning new technologies and working on side projects.
-                    I enjoy spending time with friends and family, while also doing things i love like watching movies/series, going to the gym and listening to music. </p>
+            <div className="skillsContainer">
+
+                <h1>Skills</h1>
+
+                <div className="skillsContainer-panel">
+                    <ul className="skillsContainer-box">
+                        <li><span><AiFillHtml5 class="logo" /></span> HTML5</li>
+                        <li><span><IoLogoCss3 class="logo" /></span>CSS3</li>
+                        <li><span><IoLogoJavascript class="logo" /></span>JAVASCRIPT</li>
+                        <li><span><IoLogoReact class="logo" /></span>REACT</li>
+                    </ul>
+
+                    <ul className="skillsContainer-box">
+                        <li><span><FaJava class="logo" /></span>JAVA</li>
+                        <li><span><FaDatabase class="logo" /></span>SQL</li>
+                        <li><span><IoLogoFirebase class="logo" /></span>FIREBASE</li>
+                        <li><span><FaGitAlt class="logo" /></span>GIT</li>
+                    </ul>
+                </div>
             </div>
+
         </main>
     );
 }
