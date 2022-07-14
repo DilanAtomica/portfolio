3dc0d631375e65ae980dd6aeb231a038ea182768
DilanAtomica <dilan-shwane@hotmail.com>
2022-07-14 16:07:04 +0200

    Created the about section

---
+++ src/Components/HomePage/HomePanel/HomePanel.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import "./HomePanel.css";
+import TypeWriter from "typewriter-effect";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Particles from "react-tsparticles";
+import {loadFull} from "tsparticles";
+
+
+function HomePanel(props) {
+
+
+    const particlesInit = async (main) => {
+        await loadFull(main);
+    };
+
+    const particlesLoaded = (container) => {
+
+    };
+
+    return (
+        <div className="homePanel">
+            <div className="homeContainer">
+                <div className="homeContainer-info">
+                    <h1>
+                        Hi,<br />
+                        I'm <span>Dilan</span>
+                        <TypeWriter options={{loop: true}} onInit={(typewriter) => {
+                            typewriter.typeString("#Frontend").pauseFor(2000).deleteAll().pauseFor(500)
+                                .typeString("#Gym").pauseFor(2000).deleteAll().pauseFor(500)
+                                .typeString("#Music").pauseFor(2000).deleteAll().start()
+                        }}/>
+                    </h1>
+                    <button type="button">Contact</button>
+                </div>
+                <div className="homeContainer-profile">
+                    <img alt="profile picture" src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                    <div className="profileLinksContainer">
+                        <GitHubIcon id="githubIcon" />
+                        <LinkedInIcon id="linkedInIcon" />
+                    </div>
+                </div>
+            </div>
+            <Particles width="100vw" height="100vh"
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "#0C0C0C",
+                        },
+                    },
+                    fullScreen: {
+                        enable: false,
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 100,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 2,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 50,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 3 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
+        </div>
+    );
+}
+
+export default HomePanel;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,131 +1,19 @@
 import React from 'react';
 import "./HomePage.css";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import TypeWriter from "typewriter-effect";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HomePanel from "../../Components/HomePage/HomePanel/HomePanel";
 
 function HomePage(props) {
 
-    const particlesInit = async (main) => {
-        console.log(main);
-
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        await loadFull(main);
-    };
-
-    const particlesLoaded = (container) => {
-        console.log(container);
-    };
-
-
-
     return (
         <main className="homePage">
-            <div className="homePanel">
-                <div className="homeContainer">
-                    <div className="homeContainer-info">
-                        <h1>
-                            Hi,<br />
-                            I'm <span>Dilan</span>
-                            <TypeWriter options={{loop: true}} onInit={(typewriter) => {
-                            typewriter.typeString("#Frontend").pauseFor(2000).deleteAll().pauseFor(500)
-                                .typeString("#Gym").pauseFor(2000).deleteAll().pauseFor(500)
-                                .typeString("#Music").pauseFor(2000).deleteAll().start()
-                        }}/>
-                        </h1>
-                        <button type="button">Contact</button>
-                    </div>
-                    <div className="homeContainer-profile">
-                        <img alt="profile picture" src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-                        <div className="profileLinksContainer">
-                            <GitHubIcon id="githubIcon" />
-                            <LinkedInIcon id="linkedInIcon" />
-                        </div>
-                    </div>
-                </div>
-                <Particles
-                    id="tsparticles"
-                    init={particlesInit}
-                    loaded={particlesLoaded}
-                    options={{
-                        background: {
-                            color: {
-                                value: "#0C0C0C",
-                            },
-                        },
-                        fpsLimit: 120,
-                        interactivity: {
-                            events: {
-                                onClick: {
-                                    enable: true,
-                                    mode: "push",
-                                },
-                                onHover: {
-                                    enable: true,
-                                    mode: "repulse",
-                                },
-                                resize: true,
-                            },
-                            modes: {
-                                push: {
-                                    quantity: 4,
-                                },
-                                repulse: {
-                                    distance: 200,
-                                    duration: 0.4,
-                                },
-                            },
-                        },
-                        particles: {
-                            color: {
-                                value: "#ffffff",
-                            },
-                            links: {
-                                color: "#ffffff",
-                                distance: 100,
-                                enable: true,
-                                opacity: 0.5,
-                                width: 1,
-                            },
-                            collisions: {
-                                enable: true,
-                            },
-                            move: {
-                                direction: "none",
-                                enable: true,
-                                outModes: {
-                                    default: "bounce",
-                                },
-                                random: false,
-                                speed: 2,
-                                straight: false,
-                            },
-                            number: {
-                                density: {
-                                    enable: true,
-                                    area: 800,
-                                },
-                                value: 50,
-                            },
-                            opacity: {
-                                value: 0.5,
-                            },
-                            shape: {
-                                type: "circle",
-                            },
-                            size: {
-                                value: { min: 1, max: 3 },
-                            },
-                        },
-                        detectRetina: true,
-                    }}
-                />
-            </div>
+            <HomePanel />
 
+            <div className="aboutContainer">
+                <h1>About</h1>
+                <p>My name is Dilan and I'm a 23 year old student living in Kristiansand, Norway. Im currently in my third year at University of Agder, studying IT and information-systems.
+                I have been learning frontend development next to school, every day since Januray of 2022. I love learning new technologies and working on side projects.
+                    I enjoy spending time with friends and family, while also doing things i love like watching movies/series, going to the gym and listening to music. </p>
+            </div>
         </main>
     );
 }
