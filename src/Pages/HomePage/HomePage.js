4d614e7d05c94a22007234e43b733771d461913a
DilanAtomica <dilan-shwane@hotmail.com>
2022-07-18 19:16:00 +0200

    Using EmailJS, the iinputs in the contact form will now be sent to my email

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import "./HomePage.css";
 import HomePanel from "../../Components/HomePage/HomePanel/HomePanel";
 import AboutContainer from "../../Components/HomePage/AboutContainer/AboutContainer";
 import SkillsContainer from "../../Components/HomePage/SkillsContainer/SkillsContainer";
 import ProjectsContainer from "../../Components/HomePage/ProjectsContainer/ProjectsContainer";
 
+
+import emailjs from '@emailjs/browser';
+
 function HomePage(props) {
+
+    const [inputName, setInputName] = useState("");
+    const [inputEmail, setInputEmail] = useState("");
+    const [inputText, setInputText] = useState("");
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_mba6hne', 'template_t2nsjxb', form.current, 'Hf_SVKOc5LKH9lKP8')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        setInputEmail("");
+        setInputText("");
+        setInputName("");
+
+    };
 
     return (
         <main className="homePage">
@@ -16,10 +41,10 @@ function HomePage(props) {
 
             <div className="contactContainer">
                 <h1>Contact</h1>
-                <form className="contactContainer-form">
-                    <input id="nameInput" type="text" placeholder="Name" />
-                    <input id="emailInput" type="email" placeholder="Email" />
-                    <textarea id="textInput" cols="0" rows="10" placeholder="What do you want to say?" />
+                <form onSubmit={sendEmail} ref={form} className="contactContainer-form">
+                    <input value={inputName} onChange={(e) => setInputName(e.target.value)} id="nameInput" type="text" name="user_name" placeholder="Name" />
+                    <input value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} id="emailInput" type="email" name="user_email" placeholder="Email" />
+                    <textarea value={inputText} onChange={(e) => setInputText(e.target.value)} id="textInput" cols="0" rows="10" name="message" placeholder="What do you want to say?" />
                     <button type="submit">Send</button>
                 </form>
             </div>
